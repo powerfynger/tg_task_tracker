1dad1b0aba6fcf70a2d1fc880ccf07c317e40143
powerfyng <powerfyng@mail.ru>
2024-07-14 01:43:06 +0300

    (feature) Добавлены модели БД для задач и пользователей

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+
+    tasks = db.relationship('Task', backref='owner', lazy='dynamic')
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(256))
+    hours_spent = db.Column(db.Integer, default=0)
+    deadline = db.Column(db.DateTime)
+    importance = db.Column(db.String(32))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
